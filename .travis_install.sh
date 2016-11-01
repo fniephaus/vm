8ed2511532f97d3569990901adcdfd54357ef928
Fabio Niephaus <code@fniephaus.com>
2016-11-01 14:56:06 +0100

    Try again

--- .travis_install.sh
+++ .travis_install.sh
@@ -3,21 +3,21 @@ set -e
 if [[ "${ARCH}" = "linux32x86" ]]; then
     sudo apt-get remove -q -y gvfs-daemons
     sudo apt-get install -yq --no-install-suggests --no-install-recommends --force-yes \
-            devscripts
-            libc6-dev:i386
-            libasound2:i386
-            libasound2-dev:i386
-            libssl-dev:i386
-            libssl0.9.8:i386
-            libfreetype6-dev:i386
-            libx11-dev:i386
-            libsm-dev:i386
-            libice-dev:i386
-            libgl1-mesa-glx:i386
-            libgl1-mesa-dev:i386
-            libxext-dev:i386
-            libglapi-mesa:i386
-            gcc-multilib
+            devscripts \
+            libc6-dev:i386 \
+            libasound2:i386 \
+            libasound2-dev:i386 \
+            libssl-dev:i386 \
+            libssl0.9.8:i386 \
+            libfreetype6-dev:i386 \
+            libx11-dev:i386 \
+            libsm-dev:i386 \
+            libice-dev:i386 \
+            libgl1-mesa-glx:i386 \
+            libgl1-mesa-dev:i386 \
+            libxext-dev:i386 \
+            libglapi-mesa:i386 \
+            gcc-multilib \
             uuid-dev:i386
 fi
 
